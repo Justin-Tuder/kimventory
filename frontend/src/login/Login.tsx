a56aeafee40fa45c4bf4316b10023d387d329690
Ordrik <jtuder@dtcc.edu>
2025-01-13 15:34:20 -0500

    Updated backend to work between machines

--- frontend/src/login/Login.tsx
+++ frontend/src/login/Login.tsx
@@ -1,25 +1,7 @@
-import { useState } from 'react';
-
-function Login() {
-	const [count, setCount] = useState(0);
-
+export default function Login() {
 	return (
 		<>
-			<h1>Login</h1>
-			<button className="btn btn-danger">Cancel</button>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/Login.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
+			<h1>Add a card</h1>
 		</>
 	);
 }
-
-export default Login;
